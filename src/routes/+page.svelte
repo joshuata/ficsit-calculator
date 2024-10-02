cdb20893c9f3da7d63f2f465758a2f3575b61017
Josh Asplund <joshasplund@gmail.com>
2024-10-02 14:36:33 -0500

    Add initial flow deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import ProductionCalculator from "./ProductionCalculator.svelte";
+</script>
+
+<h1>Production Calculator</h1>
+<div style:height="500px">
+    <ProductionCalculator />
+</div>
---
+++ src/routes/ProductionCalculator.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import {
+        SvelteFlow,
+        Controls,
+        Background,
+        BackgroundVariant,
+        MiniMap
+    } from '@xyflow/svelte';
+    import { writable } from 'svelte/store';
+
+    import '@xyflow/svelte/dist/style.css';
+
+    const nodes = writable([]);
+    const edges = writable([]);
+
+    
+
+</script>
+
+<SvelteFlow
+    {nodes}
+    {edges}
+    fitView
+>
+    <Controls />
+    <Background />
+
+</SvelteFlow>
